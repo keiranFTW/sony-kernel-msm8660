d4c49e55efddd1590a022f9c6c4b82dac7e8ee91
Keiran Collins <keiran@darkforestgroup.com>
2012-08-06 13:16:04 +0100

    Revert "cpufreq: interactive: add boost pulse interface"
    This reverts commit 1deee97dbcc40852f075e3cc460ca44a77db2535.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -497,6 +497,7 @@ static void cpufreq_interactive_boost(void)
 	unsigned long flags;
 	struct cpufreq_interactive_cpuinfo *pcpu;
 
+	trace_cpufreq_interactive_boost(hispeed_freq);
 	spin_lock_irqsave(&up_cpumask_lock, flags);
 
 	for_each_online_cpu(i) {
@@ -535,10 +536,8 @@ static void cpufreq_interactive_input_event(struct input_handle *handle,
 					    unsigned int type,
 					    unsigned int code, int value)
 {
-	if (input_boost_val && type == EV_SYN && code == SYN_REPORT) {
-		trace_cpufreq_interactive_boost("input");
+	if (input_boost_val && type == EV_SYN && code == SYN_REPORT)
 		cpufreq_interactive_boost();
-	}
 }
 
 static void cpufreq_interactive_input_open(struct work_struct *w)
@@ -766,35 +765,15 @@ static ssize_t store_boost(struct kobject *kobj, struct attribute *attr,
 
 	boost_val = val;
 
-	if (boost_val) {
-		trace_cpufreq_interactive_boost("on");
+	if (boost_val)
 		cpufreq_interactive_boost();
-	} else {
-		trace_cpufreq_interactive_unboost("off");
-	}
+	else
+		trace_cpufreq_interactive_unboost(hispeed_freq);
 
 	return count;
 }
 
 define_one_global_rw(boost);
-
-static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
-				const char *buf, size_t count)
-{
-	int ret;
-	unsigned long val;
-
-	ret = kstrtoul(buf, 0, &val);
-	if (ret < 0)
-		return ret;
-
-	trace_cpufreq_interactive_boost("pulse");
-	cpufreq_interactive_boost();
-	return count;
-}
-
-static struct global_attr boostpulse =
-	__ATTR(boostpulse, 0200, NULL, store_boostpulse);
 
 static struct attribute *interactive_attributes[] = {
 	&hispeed_freq_attr.attr,
@@ -804,7 +783,6 @@ static struct attribute *interactive_attributes[] = {
 	&timer_rate_attr.attr,
 	&input_boost.attr,
 	&boost.attr,
-	&boostpulse.attr,
 	NULL,
 };
 
