678fdd2861a9f165e6c7e9aa7976b62e0b24ea6f
Keiran Collins <keiran@darkforestgroup.com>
2012-08-06 13:18:40 +0100

    Revert "cpufreq: interactive: based hispeed bump on target freq, not actual"
    This reverts commit acb89115da8b8ef0925a56e546d45aff3c9950c9.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -174,7 +174,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 		cpu_load = load_since_change;
 
 	if (cpu_load >= go_hispeed_load) {
-		if (pcpu->target_freq <= pcpu->policy->min) {
+		if (pcpu->policy->cur == pcpu->policy->min) {
 			new_freq = hispeed_freq;
 		} else {
 			new_freq = pcpu->policy->max * cpu_load / 100;
